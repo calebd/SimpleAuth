1e72d3f0045bd4938616550bf23848fe58e17d6c
Caleb Davenport <calebmdavenport@gmail.com>
2013-11-07 12:58:05 -0800

    Begin implementing Facebook system provider.

--- SimpleAuth/SimpleAuthFacebookProvider.m
+++ SimpleAuth/SimpleAuthFacebookProvider.m
@@ -11,10 +11,50 @@
 
 @implementation SimpleAuthFacebookProvider
 
+#pragma mark - NSObject
+
 + (void)load {
     @autoreleasepool {
-//        [SimpleAuth registerProviderClass:self];
+        [SimpleAuth registerProviderClass:self];
     }
 }
+
+
+#pragma mark - SimpleAuthProvider
+
++ (NSString *)type {
+    return @"facebook";
+}
+
+
+#pragma mark - SimpleAuthSystemProvider
+
+- (void)loadSystemAccount:(SimpleAuthSystemAccountHandler)completion {
+    ACAccountStore *store = [[self class] accountStore];
+    ACAccountType *type = [store accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+    [store requestAccessToAccountsWithType:type options:nil completion:^(BOOL granted, NSError *error) {
+        dispatch_async(dispatch_get_main_queue(), ^{
+            if (granted) {
+                NSArray *accounts = [store accountsWithAccountType:type];
+                NSUInteger numberOfAccounts = [accounts count];
+                if (numberOfAccounts == 0) {
+                    completion(nil, error ?: [[NSError alloc] initWithDomain:ACErrorDomain code:ACErrorAccountNotFound userInfo:nil]);
+                }
+                else {
+                    completion([accounts lastObject], nil);
+                }
+            }
+            else {
+                completion(nil, error ?: [[NSError alloc] initWithDomain:ACErrorDomain code:ACErrorPermissionDenied userInfo:nil]);
+            }
+        });
+    }];
+}
+
+
+- (void)authorizeWithSystemAccount:(ACAccount *)account completion:(SimpleAuthRequestHandler)completion {
+    
+}
+
 
 @end

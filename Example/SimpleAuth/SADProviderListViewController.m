050b09f951e2281a8cfae56a2d9102e1c5757c6d
Caleb Davenport <calebmdavenport@gmail.com>
2014-10-09 14:08:13 -0400

    Finish renaming methods.

--- Example/SimpleAuth/SADProviderListViewController.m
+++ Example/SimpleAuth/SADProviderListViewController.m
@@ -77,7 +77,7 @@
         return;
     }
     
-    [SimpleAuth authenticateProvider:provider completion:^(id responseObject, NSError *error) {
+    [SimpleAuth authenticateWithProvider:provider completion:^(id responseObject, NSError *error) {
         NSLog(@"\nResponse: %@\nError: %@", responseObject, error);
     }];
 }
--- Pod/Core/SimpleAuth.h
+++ Pod/Core/SimpleAuth.h
@@ -90,7 +90,7 @@ extern NSString * const SimpleAuthRedirectURIKey;
  @param provider A single provider type.
  @param completion Called on the main queue when the operation is complete.
  */
-+ (void)authenticateProvider:(NSString * )provider completion:(SimpleAuthRequestHandler)completion;
++ (void)authenticateWithProvider:(NSString * )provider completion:(SimpleAuthRequestHandler)completion;
 
 /**
  Perform authentication with the given provider. Options provided here will
@@ -99,7 +99,7 @@ extern NSString * const SimpleAuthRedirectURIKey;
  @param provider A single provider type.
  @param completion Called on the main queue when the operation is complete.
  */
-+ (void)authenticateProvider:(NSString *)provider options:(NSDictionary *)options completion:(SimpleAuthRequestHandler)completion;
++ (void)authenticateWithProvider:(NSString *)provider options:(NSDictionary *)options completion:(SimpleAuthRequestHandler)completion;
 
 /**
  Perform authentication with the given providers. SimpleAuth will start
@@ -109,7 +109,7 @@ extern NSString * const SimpleAuthRedirectURIKey;
  @param providers An array of provider types.
  @param completion Called on the main queue when the operation is complete.
  */
-+ (void)authenticateProviders:(NSArray *)providers completion:(SimpleAuthRequestHandler)completion;
++ (void)authenticateWithProviders:(NSArray *)providers completion:(SimpleAuthRequestHandler)completion;
 
 /**
  Perform authentication with the given providers. SimpleAuth will start
@@ -120,7 +120,7 @@ extern NSString * const SimpleAuthRedirectURIKey;
  @param providers An array of provider types.
  @param completion Called on the main queue when the operation is complete.
  */
-+ (void)authenticateProviders:(NSArray *)providers options:(NSDictionary *)options completion:(SimpleAuthRequestHandler)completion;
++ (void)authenticateWithProviders:(NSArray *)providers options:(NSDictionary *)options completion:(SimpleAuthRequestHandler)completion;
 
 /**
  Determine whether the provider can handle the callback URL or not. 

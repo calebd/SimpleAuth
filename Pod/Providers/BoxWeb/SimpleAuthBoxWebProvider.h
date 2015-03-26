f67e045dfa13513dca5df72e88e16f6c5ba8b385
dkhamsing <dkhamsing@cirruspath.com>
2015-03-26 10:51:54 -0700

    Box

---
+++ Pod/Providers/BoxWeb/SimpleAuthBoxWebProvider.h
@@ -0,0 +1,13 @@
+//
+//  SimpleAuthBoxWebProvider.h
+//  SimpleAuth
+//
+//  Created by dkhamsing on 3/26/15.
+//  Copyright (c) 2015 dkhamsing. All rights reserved.
+//
+
+#import "SimpleAuthProvider.h"
+
+@interface SimpleAuthBoxWebProvider : SimpleAuthProvider
+
+@end
